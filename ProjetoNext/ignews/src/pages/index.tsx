c17c7ae13748cf38e6aeb71b5222628bc251ee11
Marcos Vini/trabalho <viniciusmg99@outlook.com>
2022-04-15 23:51:37 -0400

    Finalizando a interface / Integrando com a API Stripe

--- ProjetoNext/ignews/src/pages/index.tsx
+++ ProjetoNext/ignews/src/pages/index.tsx
@@ -1,12 +1,23 @@
+import { GetStaticProps } from 'next';
+
 import Head from 'next/head';
 
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
 import styles from './home.module.scss';
 
 
+// criando a tipagem com o typeScript
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
 
-export default function Home() {
+
+export default function Home({ product }) {
     return (
       <>
         <Head>
@@ -20,9 +31,9 @@ export default function Home() {
                 <h1>News about the <span>React</span> world.</h1>
                 <p>
                   Get acess to all the publications <br />
-                    <span>for $9.90 month</span>
+                    <span>for {product.amount} month</span>
                 </p>
-                <SubscribeButton />
+                <SubscribeButton priceId={product.priceId} />
               </section>
 
               <img src="/images/avatar.svg" alt="Girl coding" />
@@ -34,3 +45,22 @@ export default function Home() {
     )
 }
 
+
+export const getStaticProps: GetStaticProps  = async () => {
+  const price = await stripe.prices.retrieve('price_1KohLZJA4YObdWpiKHnb9D16')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  };
+  
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, // min, segundos, 24 para um dia
+  }
+}
